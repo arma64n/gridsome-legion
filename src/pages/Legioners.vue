b06472009538c3b657febb2b000daf6c9ac3ed6e
arma64n <arma64n@gmail.com>
2020-04-22 17:41:43 +0600

    initial commit

---
+++ src/pages/Legioners.vue
@@ -0,0 +1,35 @@
+<template>
+  <Layout>
+    <h1>Legioners</h1>
+    <ul>
+      <li v-for="legioner in $page.allStrapiLegioners.edges">
+        {{ legioner.node.title }}
+        {{ legioner.node.matches.length }}
+      </li>
+    </ul>
+  </Layout>
+</template>
+
+<page-query>
+{
+  allStrapiLegioners {
+    totalCount
+    edges {
+      node {
+        title
+        matches {
+          id
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Legioners",
+  },
+};
+</script>
---
+++ src/pages/Matches.vue
@@ -0,0 +1,38 @@
+<template>
+  <Layout>
+    <h1>Matches</h1>
+    <ul>
+      <li v-for="match in $page.allStrapiMatches.edges">
+        {{ match.node.date }}
+        {{ match.node.city.title }}
+        {{ match.node.opponent.title }}
+      </li>
+    </ul>
+  </Layout>
+</template>
+
+<page-query>
+{
+allStrapiMatches {
+  edges {
+    node {
+      date
+      city {
+        title
+      }
+      opponent {
+        title
+      }
+    }
+  }
+}
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Matches",
+  },
+};
+</script>
